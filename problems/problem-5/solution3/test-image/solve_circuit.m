a9608a14baf9a3f2f540dea75d7399e006f38ce0
3-rapat <theerapat.p@codefin.io>
2026-08-08 15:45:43 +0700

    fix(problem-5): update test-image solution to exact user convention (Branch 4: e->b, Q_f = [[1,0,-1,1],[0,1,0,1]])

--- problems/problem-5/solution3/test-image/solve_circuit.m
+++ problems/problem-5/solution3/test-image/solve_circuit.m
@@ -1,4 +1,4 @@
-% Solve Circuit Problem 5 using Fundamental Cut-set Method (4 Branches: Tree = {1, 2}, Links = {3, 4})
+% Solve Circuit Problem 5 using Fundamental Cut-set Method (User Convention: Q_f = [[1,0,-1,1],[0,1,0,1]])
 function res = solve_circuit(G1, G2, G3, G4, E1, E2, E3)
     if nargin < 7, E3 = 2.0; end
     if nargin < 6, E2 = 6.0; end
@@ -9,16 +9,16 @@ function res = solve_circuit(G1, G2, G3, G4, E1, E2, E3)
     if nargin < 1, G1 = 0.4; end
 
     Vb = -E2;
-    Va = (G1*E1 - G2*(E2 + E3)) / (G1 + G2 + G3);
+    Va = (G1*E1 - G2*E3 - G2*E2) / (G1 + G2 + G3);
     
     i1 = G1*(E1 - Va);
     i2 = G2*(Va + E3 - Vb);
     i3 = G3*Va;
     iG4 = -G4*Vb;
-    iE2 = i2 - iG4;
+    iE2 = -i2 - iG4;
     i4 = iG4 + iE2;
     
-    fprintf('=== Solution for Problem 5 (4 Branches: Tree = {1, 2}, Links = {3, 4}) ===\n');
+    fprintf('=== Solution for Problem 5 (User Convention) ===\n');
     fprintf('Va = %.6f V\n', Va);
     fprintf('Vb = %.6f V\n', Vb);
     fprintf('i1 = %.6f A\n', i1);
